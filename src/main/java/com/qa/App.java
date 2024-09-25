d42a99d128b3b9cc6cd41c7c0fc63004d44eabd3
jiya bharti <jiyabharti@outlook.com>
2024-09-25 16:32:52 +0100

    added webhook

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+             System.out.println(sayHelloToSomeone("Jiya"));
         System.out.println(sayGoodbye());
     }
 
